b0fb431d9c31e61633abf6c852ca8dbe25a73152
urraccon <133591634+urraccon@users.noreply.github.com>
2023-11-15 07:46:07 +0200

    added the working files

---
+++ js/01-gallery.js
@@ -0,0 +1,10 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((picture) => {
+  const listItem = document.createElement("li");
+  const referenceItem = document.createElement("a");
+  const imageItem = document.createEleemnt("img");
+});
+console.log(galleryItems);
