c09b2e7dfd4bd43576e5cf7c0933ecdba6b2d87e
urraccon <133591634+urraccon@users.noreply.github.com>
2023-11-19 19:23:51 +0200

    created a gallery each using simplelightbox and basiclightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,39 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+console.log(galleryItems);
+
 const gallery = document.querySelector(".gallery");
 
-galleryItems.forEach((picture) => {
-  const listItem = document.createElement("li");
-  const referenceItem = document.createElement("a");
-  const imageItem = document.createEleemnt("img");
+galleryItems.forEach((image) => {
+  const liTag = document.createElement("li");
+  liTag.classList.add("gallery__item");
+  gallery.append(liTag);
+  const aTag = document.createElement("a");
+  aTag.classList.add("gallery__link");
+  aTag.href = image.original;
+  liTag.append(aTag);
+  const imgTag = document.createElement("img");
+  imgTag.classList.add("gallery__image");
+  imgTag.src = image.preview;
+  imgTag.dataset.source = image.original;
+  imgTag.alt = image.description;
+  aTag.append(imgTag);
 });
-console.log(galleryItems);
+
+gallery.addEventListener("click", openImage);
+
+function openImage(event) {
+  const clickedOnImage = event.target;
+  console.dir(clickedOnImage);
+  // debugger;
+  if (clickedOnImage.tagName === "IMG") {
+    console.log("ai apasat pe imagine");
+    event.preventDefault();
+    const originalImage = basicLightbox.create(
+      `
+    <img src=${clickedOnImage.dataset.source}>`
+    );
+    originalImage.show();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  const liTag = document.createElement("li");
+  liTag.classList.add("gallery__item");
+  gallery.append(liTag);
+  const aTag = document.createElement("a");
+  aTag.classList.add("gallery__link");
+  aTag.href = image.original;
+  liTag.append(aTag);
+  const imgTag = document.createElement("img");
+  imgTag.classList.add("gallery__image");
+  imgTag.src = image.preview;
+  imgTag.alt = image.description;
+  aTag.append(imgTag);
+});
+
+let album = new SimpleLightbox(".gallery__link", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionDelay: "250",
+});
